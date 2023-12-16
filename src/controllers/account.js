8b91e4d3045fc3789e4a8da4015bbb0e29e7140a
Luccasmarinho <luccas88oliveira@gmail.com>
2023-12-15 23:50:59 -0300

    Inserindo função para inserir usuario

--- src/controllers/account.js
+++ src/controllers/account.js
@@ -1,16 +1,36 @@
 const knex = require("../database/connection");
-const { messagejson } = require("../utils/utils");
+const { messagejson, verifySingleField } = require("../utils/utils");
+const { hash } = require("bcrypt")
 
 const getAllAccounts = async (req, res) => {
     // return messagejson(res, 200, "ok")
 }
 
+
 const createAccount = async (req, res) => {
     const { body: { nome, cpf, data_nascimento, email, telefone, senha } } = req
-    
+
     try {
-       return messagejson(res, 200, "ok")
+        const userExists = await verifySingleField("usuarios", req.body, [email, cpf])
+        const { status, message } = userExists
+
+        if (status) return messagejson(res, 400, message)
+
+        const encryptedPass = await hash(senha, 10)
+
+        delete req.body.senha
+
+        const user = {
+            ...req.body,
+            senha: encryptedPass
+        }
+
+        const [insertUser] = await knex("usuarios").insert(user).returning("*")
+
+        delete insertUser.senha   
+       return messagejson(res, 200, insertUser)
     } catch (error) {
+        // console.log(error);
         return messagejson(res, 500, "Erro interno do servidor.")
     }
 }

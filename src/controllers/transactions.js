4533891ebfab72ff183ab37d8ca8f964b0a5733c
Luccasmarinho <luccas88oliveira@gmail.com>
2023-12-17 17:47:22 -0300

    Ajustes + função de sacar

--- src/controllers/transactions.js
+++ src/controllers/transactions.js
@@ -1,20 +1,19 @@
 const knex = require("../database/connection");
-const { messageJson, formatDate } = require("../utils/utils");
+const { messageJson, compareId, insertValue } = require("../utils/utils");
+const { compare } = require("bcrypt");
+
 
 const deposit = async (req, res) => {
     const { body: { numero_conta, valor }, user: { id } } = req
 
-    if (id !== numero_conta) return messageJson(res, 404, "Número da conta incorreto.")
+    const { status, message } = compareId(id, numero_conta)
+
+    if (!status) return messageJson(res, 400, message)
 
     try {
         await knex.raw(`UPDATE usuarios SET saldo = saldo + ${valor} WHERE id = ${id}`)
 
-        const objInsert = {
-            numero_conta,
-            valor,
-            data: formatDate()
-        }
-        const insertDeposit = await knex("depositar").insert(objInsert).returning("*")
+        const [insertDeposit] = await insertValue("depositar", numero_conta, valor)
 
         return messageJson(res, 200, insertDeposit)
     } catch (error) {
@@ -23,7 +22,28 @@ const deposit = async (req, res) => {
 }
 
 const withdraw = async (req, res) => {
+    const { body: { numero_conta, valor, senha }, user: { id, saldo } } = req
 
+    const { status, message } = compareId(id, numero_conta)
+
+    if (!status) return messageJson(res, 400, message)
+    try {
+        const [row] = await knex("usuarios").where({ id })
+
+        const truePass = await compare(senha, row.senha)
+
+        if(!truePass) return messageJson(res, 400, "Senha incorreta. Verifique sua senha e tente novamente.")
+
+        if(valor > saldo) return messageJson(res, 400, "Saldo insuficiente para completar essa operação.")
+        
+        await knex.raw(`UPDATE usuarios SET saldo = saldo - ${valor} where id = ${id}`)
+
+        const [insertWithdraw] = await insertValue("sacar", numero_conta, valor)
+
+        return messageJson(res, 200, insertWithdraw)
+    } catch (error) {
+        return messageJson(res, 500, "Erro interno do servidor.")
+    }
 }
 
 const transfer = async (req, res) => {

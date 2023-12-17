7d85c058d4fe4f2da175de874afb8a1cef78df87
Luccasmarinho <luccas88oliveira@gmail.com>
2023-12-17 17:45:51 -0300

    Add funções no utils

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -14,37 +14,37 @@ const utils = {
     verifySingleField: async (table, objReq, arrField) => {
         const arrayObjReq = Object.entries(objReq)
         const arrayValues = []
-    
+
         arrayObjReq.forEach((i) => {
             arrField.forEach((j) => {
                 if (i[1] == j) arrayValues.push(i)
             })
         })
-    
+
         const columnName = []
         let arrayQuery = []
-    
+
         for (let i = 0; i < arrayValues.length; i++) {
-    
+
             const queryVerify = await knex.raw(`select * from ${table} where ${arrayValues[i][0]} = '${arrayValues[i][1]}'`)
-    
+
             if (queryVerify.rows.length > 0) columnName.push(arrayValues[i][0])
-    
+
             const [query] = queryVerify.rows
             if (query) arrayQuery.push(query)
         }
-    
+
         const status = arrayQuery.length == 0 ? false : true
         const message = arrayQuery.length == 0
             ? `Nenhum campo cadastrado no banco de dados.`
             : `Campos já cadastrados no banco de dados: ${columnName.join(", ").toUpperCase()}`
-    
+
         const result = {
             query: arrayQuery,
             status,
             message
         }
-    
+
         return result
     },
     formatDate: () => {
@@ -53,6 +53,32 @@ const utils = {
         const result = format(date, "dd/MM/yyyy, HH:mm:ss")
 
         return result
+    },
+    compareId: (id, numberAccount) => {
+        let status = false
+        if (id == numberAccount) status = true
+
+        const message = !status
+            ? "Número da conta incorreto."
+            : "Número da conta correspondem."
+
+        const result = {
+            status,
+            message
+        }
+
+        return result
+    },
+    insertValue: async (table, numberAcc, value) => {
+        const objInsert = {
+            numero_conta: numberAcc,
+            valor: value,
+            data: utils.formatDate()
+        }
+
+        const insert = await knex(`${table}`).insert(objInsert).returning("*")
+
+        return insert
     }
 }
 

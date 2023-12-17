7e27c4beea83e482cb224b5cb9a0f67834ad3d19
Luccasmarinho <luccas88oliveira@gmail.com>
2023-12-17 02:53:30 -0300

    add deposit e mais alguns erros

--- src/schemas/schemaUser.js
+++ src/schemas/schemaUser.js
@@ -13,6 +13,10 @@ const schemas = {
         email: joi.string().email().required(),
         senha: joi.string().min(8).max(20).required()
     }),
+    deposit: joi.object({
+        numero_conta: joi.number().positive().integer().required(),
+        valor: joi.number().min(1).integer().required()
+    }),
     error: {
         "string.empty": `O campo '$' não pode estar vazio`,
         "any.required": `O campo '$' é obrigatório`,
@@ -21,6 +25,10 @@ const schemas = {
         "string.max": `O campo '$' precisa ter no máximo`,
         "object.unknown": `O campo '$' é inválido`,
         "string.email": `Digite um '$' válido`,
+        "number.base": "O campo '$' precisa ser um número",
+        "number.positive": "O campo '$' precisa ser um número positivo",
+        "number.integer": "O campo '$' precisa ser um número inteiro",
+        "number.min": "O '$' precisa ser de no mínimo 1",
     }
 }
 

153a5779f7415df0bee969c905a169c3c14005ca
Luccasmarinho <luccas88oliveira@gmail.com>
2023-12-16 21:42:39 -0300

    Rota de login

--- src/router/router.js
+++ src/router/router.js
@@ -3,10 +3,11 @@ const router = express();
 const login = require("../controllers/login")
 const { getAllAccounts, createAccount } = require("../controllers/account");
 const { validateBody } = require("../middlewares/validateBody");
-const { schemaUser } = require("../schemas/schemaUser")
+const schemas = require("../schemas/schemaUser")
 
 
-router.post("/contas", validateBody(schemaUser), createAccount)
+router.post("/contas", validateBody(schemas.user), createAccount)
+router.post("/login", validateBody(schemas.login), login)
 router.get("/contas", getAllAccounts)
 
 module.exports = router

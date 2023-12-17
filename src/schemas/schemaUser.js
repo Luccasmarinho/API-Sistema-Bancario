fd77d03f4be8842bf27bdd037dba7b9c5fb1551f
Luccasmarinho <luccas88oliveira@gmail.com>
2023-12-17 17:46:52 -0300

    Inserindo schema de sacar

--- src/schemas/schemaUser.js
+++ src/schemas/schemaUser.js
@@ -17,6 +17,11 @@ const schemas = {
         numero_conta: joi.number().positive().integer().required(),
         valor: joi.number().min(1).integer().required()
     }),
+    withdraw: joi.object({
+        numero_conta: joi.number().positive().integer().required(),
+        valor: joi.number().min(1).integer().required(),
+        senha: joi.string().min(8).max(20).required()
+    }),
     error: {
         "string.empty": `O campo '$' não pode estar vazio`,
         "any.required": `O campo '$' é obrigatório`,
@@ -33,28 +38,3 @@ const schemas = {
 }
 
 module.exports = schemas
-
-// const objError = {
-//     "string.empty": `O campo '$' não pode estar vazio`,
-//     "any.required": `O campo '$' é obrigatório`,
-//     "string.base": `O campo '$' precisa ser um texto`,
-//     "string.min": `O campo '$' precisa ter no mínimo`,
-//     "string.max": `O campo '$' precisa ter no máximo`,
-//     "object.unknown": `O campo '$' é inválido`,
-//     "string.email": `Digite um '$' válido`,
-// }
-
-// const schemaUser = joi.object({
-//     nome: joi.string().required(),
-//     cpf: joi.string().min(11).max(11).required(),
-//     data_nascimento: joi.string().required(),
-//     email: joi.string().email().required(),
-//     telefone: joi.string().required(),
-//     senha: joi.string().min(8).max(20).required()
-// });
-
-    // module.exports = {
-    //     schemaUser,
-    //     objError
-    // }
-

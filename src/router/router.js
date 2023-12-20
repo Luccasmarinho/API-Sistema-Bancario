45a602c11f8f55bcd2c05cf8eaae714c1c7f338c
Luccasmarinho <luccas88oliveira@gmail.com>
2023-12-19 23:51:19 -0300

    Implementando middleware de validação de senha

--- src/router/router.js
+++ src/router/router.js
@@ -1,21 +1,24 @@
 const express = require("express");
 const router = express();
 
-const login = require("../controllers/login");
 const schemas = require("../schemas/schemaUser");
+
 const tokenAuthentication = require("../middlewares/tokenAuthenticate");
-const { getAllAccounts, createAccount } = require("../controllers/account");
+const validatePass = require("../middlewares/validatePass");
 const { validateBody } = require("../middlewares/validateBody");
+
+const login = require("../controllers/login");
+const { getAllAccounts, createAccount } = require("../controllers/account");
 const { deposit, withdraw, transfer } = require("../controllers/transactions");
 
 
 router.post("/contas", validateBody(schemas.user), createAccount)
-router.post("/login", validateBody(schemas.login), login)
+router.post("/login", validateBody(schemas.login), validatePass, login)
 
 // router.get("/contas", tokenAuthentication, getAllAccounts)
 router.use(tokenAuthentication)
 router.post("/transacoes/depositar", validateBody(schemas.deposit), deposit)
-router.post("/transacoes/sacar", validateBody(schemas.withdraw), withdraw)
-router.post("/transacoes/transferir", validateBody(schemas.transfer), transfer)
+router.post("/transacoes/sacar", validateBody(schemas.withdraw), validatePass, withdraw)
+router.post("/transacoes/transferir", validateBody(schemas.transfer), validatePass, transfer)
 
 module.exports = router

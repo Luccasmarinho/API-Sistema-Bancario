cab2841799bc2be7ddf3c4d1f5c11ec825f87647
Luccasmarinho <luccas88oliveira@gmail.com>
2023-12-22 16:16:11 -0300

    add cors

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const server = express();
+const cors = require("cors");
 const router = require("./router/router");
 
 server.use(express.json());
 server.use(router);
+server.use(cors());
 
 const port = process.env.PORT || 3000
 server.listen(port, () => console.log("Server ON"));

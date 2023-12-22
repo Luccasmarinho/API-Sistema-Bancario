1034f6fed8c5c3402b63f1c2b76a6c5f36183f54
Luccas Marinho <138074678+Luccasmarinho@users.noreply.github.com>
2023-12-22 16:22:52 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,9 +4,9 @@ const server = express();
 const cors = require("cors");
 const router = require("./router/router");
 
+server.use(cors());
 server.use(express.json());
 server.use(router);
-server.use(cors());
 
 const port = process.env.PORT || 3000
 server.listen(port, () => console.log("Server ON"));
